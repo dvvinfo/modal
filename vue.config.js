2dd9b2c6b59f04c1fdf050ce0161310bd6c75ee7
Vladimir Drobyshev <dvvinfo@mail.ru>
2021-01-14 19:52:11 +0300

    Production version

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // options...
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
-    : '/',
+    ? ''
+    : '',
   assetsDir: '',
   filenameHashing: true,
   productionSourceMap: false
